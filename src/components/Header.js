0faba9152875e2e15cc3a0ff9f70365b23a27dcb
leefreemanxyz <lee.freeman1@gmail.com>
2017-07-25 22:52:08 +0200

    google analytics and header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,22 @@ const styles = {
   header: {
     backgroundColor: "#009688",
     width: "100hw",
-    height: "50px"
+    height: "50px",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  siteTitle:{
+    color:"white",
   }
 }
 
 export default class Header extends PureComponent {
   render(){
     return (
-      <nav style={styles.header}></nav>
+      <nav style={styles.header}>
+        <h1 style={styles.siteTitle}>MusicVisualizr</h1>
+      </nav>
     )
   }
 }
