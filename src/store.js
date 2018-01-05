590949043639d89221a19da7f987e914004cc63b
leefreemanxyz <lee.freeman1@gmail.com>
2018-01-05 22:11:35 +0100

    switch from browser history to hash history

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import ReduxThunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
+import createHashHistory from 'history/createHashHistory'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 
 import reducers from './reducers'
 
-export const history = createHistory()
+export const history = createHashHistory()
 
 const routingMiddleware = routerMiddleware(history)
 const reducer = combineReducers({ ...reducers,  routing: routerReducer })
